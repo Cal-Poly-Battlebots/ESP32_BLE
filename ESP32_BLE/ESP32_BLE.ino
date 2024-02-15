6f60d0ff2ef468d7e505c907abc92a716b06488f
ShiftingKill <rosenaaron@yahoo.com>
2024-02-15 14:01:03 -0800

    Added support for Adafruit 9-DoF IMU

--- ESP32_BLE/ESP32_BLE.ino
+++ ESP32_BLE/ESP32_BLE.ino
@@ -16,7 +16,10 @@
 #include <BLE2902.h>
 #include <RoboClaw.h>
 #include <math.h>
-#include "ICM_20948.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 //---------RoboClaw----------------
 RoboClaw roboclaw(&Serial2, 10000);
@@ -33,7 +36,12 @@ RoboClaw roboclaw(&Serial2, 10000);
 // On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
 #define AD0_VAL 1
 
-ICM_20948_I2C myICM;
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(0x28);
 
 //---------BLE---------------------
 BLECharacteristic *joystickCharacteristic;
@@ -56,12 +64,24 @@ int maxMotorPower = 127;
 #define SWIPE_UUID       "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 #define ABILITY_UUID     "6E400004-B5A3-F393-E0A9-E50E24DCCA9E"
 
+/*
+   Kills motors and delays for ~1s
+*/
+void killMotors() {
+  roboclaw.ForwardM2(address_left, 0);
+  roboclaw.ForwardM1(address_right, 0);
+  roboclaw.ForwardM1(address_left, 0);
+  roboclaw.ForwardM2(address_right, 0);
+  delay(1000);
+}
+
 class MyServerCallbacks : public BLEServerCallbacks {
     void onConnect(BLEServer *pServer) {
       Serial.println("Device connected.");
       deviceConnected = true;
     };
     void onDisconnect(BLEServer *pServer) {
+      killMotors();
       Serial.println("Device disconnected.");
       deviceConnected = false;
       pServer->getAdvertising()->start(); // Restart advertising
@@ -108,71 +128,48 @@ class MyReadCallbacks : public BLECharacteristicCallbacks {
 };
 
 float readIMU() {
-  // Read any DMP data waiting in the FIFO
-  // Note:
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFONoDataAvail if no data is available.
-  //    If data is available, readDMPdataFromFIFO will attempt to read _one_ frame of DMP data.
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOIncompleteData if a frame was present but was incomplete
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_Ok if a valid frame was read.
-  //    readDMPdataFromFIFO will return ICM_20948_Stat_FIFOMoreDataAvail if a valid frame was read _and_ the FIFO contains more (unread) data.
-  icm_20948_DMP_data_t data;
-  myICM.readDMPdataFromFIFO(&data);
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
 
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
-  {
-    //SERIAL_PORT.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
-    //if ( data.header < 0x1000) SERIAL_PORT.print( "0" ); // Pad the zeros
-    //if ( data.header < 0x100) SERIAL_PORT.print( "0" );
-    //if ( data.header < 0x10) SERIAL_PORT.print( "0" );
-    //SERIAL_PORT.println( data.header, HEX );
+  /* Board layout:
+         +----------+
+         |         *| RST   PITCH  ROLL  HEADING
+     ADR |*        *| SCL
+     INT |*        *| SDA     ^            /->
+     PS1 |*        *| GND     |            |
+     PS0 |*        *| 3VO     Y    Z-->    \-X
+         |         *| VIN
+         +----------+
+  */
 
-    if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
-    {
-      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
-      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
-      // The quaternion data is scaled by 2^30.
+  /* The processing sketch expects data as roll, pitch, heading */
+//  Serial.print(F("Orientation: "));
+//  Serial.print((float)event.orientation.x);
+//  Serial.print(F(" "));
+//  Serial.print((float)event.orientation.y);
+//  Serial.print(F(" "));
+//  Serial.print((float)event.orientation.z);
+//  Serial.println(F(""));
 
-      //SERIAL_PORT.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
+  /* Also send calibration data for each sensor. */
+  uint8_t sys, gyro, accel, mag = 0;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+//  Serial.print(F("Calibration: "));
+//  Serial.print(sys, DEC);
+//  Serial.print(F(" "));
+//  Serial.print(gyro, DEC);
+//  Serial.print(F(" "));
+//  Serial.print(accel, DEC);
+//  Serial.print(F(" "));
+//  Serial.println(mag, DEC);
 
-      // Scale to +/- 1
-      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+//
+//  Serial.print("Facing Angle: ");
+//  Serial.println(event.orientation.x);
 
-      // Convert the quaternions to Euler angles (roll, pitch, yaw)
-      // https://en.wikipedia.org/w/index.php?title=Conversion_between_quaternions_and_Euler_angles&section=8#Source_code_2
-
-      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
-
-      double q2sqr = q2 * q2;
-
-      // roll (x-axis rotation)
-      double t0 = +2.0 * (q0 * q1 + q2 * q3);
-      double t1 = +1.0 - 2.0 * (q1 * q1 + q2sqr);
-      double roll = atan2(t0, t1) * 180.0 / PI;
-
-      // pitch (y-axis rotation)
-      double t2 = +2.0 * (q0 * q2 - q3 * q1);
-      t2 = t2 > 1.0 ? 1.0 : t2;
-      t2 = t2 < -1.0 ? -1.0 : t2;
-      double pitch = asin(t2) * 180.0 / PI;
-
-      // yaw (z-axis rotation)
-      double t3 = +2.0 * (q0 * q3 + q1 * q2);
-      double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
-      double yaw = atan2(t3, t4) * 180.0 / PI;
-
-      Serial.print("Yaw: ");
-      Serial.println(yaw);
-      return yaw;
-    }
-  }
-
-  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
-  {
-    delay(10);
-  }
-  return 0;
+  return event.orientation.x;
 }
 
 void setup() {
@@ -209,83 +206,44 @@ void setup() {
   Serial.println("Waiting for a client connection...");
 
   // Initialize RoboClaws with baud rate
-  roboclaw.begin(38400);// Should we go to 460800 bps
+  roboclaw.begin(38400);
 
-  // Initialize IMU
-  WIRE_PORT.begin();
-  WIRE_PORT.setClock(400000);
-  
-  bool initialized = false;
-  while (!initialized) {
-    // Try calling begin()
-    myICM.begin(WIRE_PORT, AD0_VAL);
-
-    // Check status and reconnect if needed
-    if (myICM.status != ICM_20948_Stat_Ok) {
-      Serial.println("Trying to connect again...");
-      delay(500);
-    } else {
-      initialized = true;
-    }
+  // ------------ Initialize IMU -----------
+  /* Initialize the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
   }
-  
-  Serial.println(myICM.statusString());
-  
-  bool success = true; // Use success to show if the DMP configuration was successful
 
-  // Initialize the DMP. initializeDMP is a weak function. You can overwrite it if you want to e.g. to change the sample rate
-  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
+  /* Use external crystal for better accuracy */
+  bno.setExtCrystalUse(true);
 
-  // Enable the DMP Game Rotation Vector sensor
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok);
-
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to the maximum
-
-  // Enable the FIFO
-  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
-
-  // Enable the DMP
-  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
-
-  // Reset DMP
-  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
-
-  // Reset FIFO
-  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
-
-  // Check success
-  if (success) {
-    Serial.println("DMP enabled!");
-  } else {
-    Serial.println("Enable DMP failed!");
-    Serial.println("Please check that you have uncommented line 29 (#define ICM_20948_USE_DMP) in ICM_20948_C.h...");
-    while (1)
-      ; // Do nothing more
-  }
-  
 }
 
 void loop() {
-  // Read IMU first
-  float angleFacing = readIMU();
-  // Adjust -180 to 180 ---> 0 to 360
-  if (angleFacing < 0) {
-    angleFacing += 360;
+  if (!deviceConnected) {
+    killMotors();
+    return;
   }
-  
+  // Read IMU first get angle 0-360
+  float angleFacing = readIMU();
+
   // Mecanum drive based on the BLE readings
   float power = joystickMagnitude / maxMagnitude;// Normalize power 0-1
   float anglesToRadians = 0.01745329252;
-  float controllerAngle = ((joystickAngle * anglesToRadians) - (PI / 4));
   // 360 values of precision
-  int finalAngle = (int) round((controllerAngle - angleFacing)) % 360;
+  int trueAngle = (int) round((joystickAngle - angleFacing)) % 360;
+  float finalAngle = ((trueAngle * anglesToRadians) - (PI / 4));
+
   float sine = sin(finalAngle);
   float cosine = cos(finalAngle);
   float maximum = max(abs(sine), abs(cosine));// 0 to 1
 
   // sine yields -1.0 to 1.0, swipe magnitude is 0-50, then -50.0 to 50.0 is normalized to -1.0 to 1.0
   float turn = sin((swipeAngle * anglesToRadians)) * swipeMagnitude / maxMagnitude;
-  int turnNormalized = turn * maxMotorPower * TURN_RATIO;// -maxMotorPower to maxMotorPower
+  int turnNormalized = turn * maxMotorPower * TURN_RATIO;// -maxMotorPower to maxMotorPower, scaled by defined amount
 
   // Motor speeds 0 to max motor power
   // Negative sign because motors are on the outside
